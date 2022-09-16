89190bff30296e02be18e5949d150c638c3784f0
harith.zainudin <harithzainudin7@gmail.com>
2022-09-17 00:26:37 +0800

    init get image & upload image

---
+++ backend/functions/get-image/handler.js
@@ -0,0 +1,8 @@
+const { initializeContext } = require("../utils/logger");
+const { okResponse } = require("../utils/response");
+
+module.exports.lambda_handler = async (event, context) => {
+  initializeContext(event, context);
+
+  return okResponse("get-image", { event, context });
+};
