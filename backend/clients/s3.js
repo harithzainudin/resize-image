6fae9ba3a0cee981cbcfb513a6c328ba09c49dc8
harith.zainudin <harithzainudin7@gmail.com>
2023-05-19 20:50:14 +0800

    add generateGerPresignedUrl function for s3 clients

--- backend/clients/s3.js
+++ backend/clients/s3.js
@@ -1,15 +1,20 @@
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const {
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+} = require("@aws-sdk/client-s3");
 const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
 
-const client = new S3Client();
+const client = new S3Client({ region: process.env.AWS_REGION });
 
 async function generatePutPresignedUrl(bucketName, key) {
-  const command = new PutObjectCommand({
-    Bucket: bucketName,
-    Key: key,
-  });
-
+  const command = new PutObjectCommand({ Bucket: bucketName, Key: key });
   return await getSignedUrl(client, command, { expiresIn: 3600 });
 }
 
-module.exports = { generatePutPresignedUrl };
+async function generateGetPresignedUrl(bucketName, key) {
+  const command = new GetObjectCommand({ Bucket: bucketName, Key: key });
+  return await getSignedUrl(client, command, { expiresIn: 3600 });
+}
+
+module.exports = { generatePutPresignedUrl, generateGetPresignedUrl };
